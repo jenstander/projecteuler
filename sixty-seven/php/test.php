07373b472441f4afa703edb906d29f8a9b43f1e6
Jen Stander <jennifer.stander@gmail.com>
2012-09-03 15:20:30 -0500

    changed variable name to make more sense, that the the rest of them do

--- sixty-seven/php/test.php
+++ sixty-seven/php/test.php
@@ -1,14 +1,14 @@
 <?php
 $handle = @fopen("input.txt", "r");
-$ass_array = array();
+$tri_array = array();
 while (($buffer = fgets($handle)) !== false){
-	$ass_array[] = explode(" ", $buffer);
+	$tri_array[] = explode(" ", $buffer);
 }
-$x = count($ass_array)- 1;
-$new_row = $ass_array[$x];
+$x = count($tri_array)- 1;
+$new_row = $tri_array[$x];
 while($x>0){
 	$x--;
-	$row = $ass_array[$x];
+	$row = $tri_array[$x];
 	$temp_arr = array();
 	foreach($row as $key=>$value){
 		$temp_arr[] = $new_row[$key] > $new_row[$key+1] ? $value + $new_row[$key] : $value + $new_row[$key+1];
